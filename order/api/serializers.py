99d702eaa3c41c2a56519eec3a255e89e0a59468
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-27 14:26:42 +0100

    Update templates

--- order/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api.models import Order
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
