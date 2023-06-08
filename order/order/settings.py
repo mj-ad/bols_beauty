ce5bd508ff338852cfc1fae0a7a0a60beee02ad7
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-06-08 20:21:43 +0100

    Changes to be committed: 	modified:   order/order/settings.py 	modified:   order/templates/order.js

--- order/order/settings.py
+++ order/order/settings.py
@@ -45,7 +45,13 @@ INSTALLED_APPS = [
     'phonenumber_field',
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +59,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'order.urls'
